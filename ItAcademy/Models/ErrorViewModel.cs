32f974f208e32ee116e8201e84fcec74082463ae
dambosin <dambosin@gmail.com>
2023-02-09 23:31:41 +0300

    Created

---
+++ ItAcademy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ItAcademy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
